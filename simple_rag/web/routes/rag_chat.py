ee5e396eecbabc57c13c2ebc572580238c92f454
Timur Mullayanov <timur.r.mullayanov@gmail.com>
2025-03-10 10:56:12 +0300

    Rename endpoint

--- simple_rag/web/routes/rag_chat.py
+++ simple_rag/web/routes/rag_chat.py
@@ -59,8 +59,7 @@ async def send_message(
         raise HTTPException(status_code=404, detail="Chat not found")
 
 
-# FIXME: get rid of /update_prompt, make model update more generic
-@router.post("/update_prompt")
+@router.post("/update_model")
 async def update_model(
     request: UpdateModelRequest,
     chat_manager: ChatManager = Depends(get_chat_manager),
