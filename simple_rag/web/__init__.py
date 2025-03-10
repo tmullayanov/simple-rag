b2403c5a1dc78ab5a05e10752c015ad608f4458c
Timur Mullayanov <timur.r.mullayanov@gmail.com>
2025-03-10 18:35:38 +0300

    rm /example

--- simple_rag/web/__init__.py
+++ simple_rag/web/__init__.py
@@ -2,16 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 
 from .contextmanager import startup_and_shutdown
-from .routes import example_router, qna_router, rag_assistant_router
+from .routes import qna_router, rag_assistant_router
 from .config import APP_SETTINGS
 
 
 app = FastAPI(lifespan=startup_and_shutdown)
 
 
-app.include_router(example_router, tags=["test"])
-app.include_router(qna_router, tags=['rag', 'qna'])
-app.include_router(rag_assistant_router, tags=['rag', 'assistant'])
+app.include_router(qna_router, tags=["rag", "qna"])
+app.include_router(rag_assistant_router, tags=["rag", "assistant"])
 
 
 def run():
--- simple_rag/web/routes/__init__.py
+++ simple_rag/web/routes/__init__.py
@@ -1,3 +1,2 @@
-from .example import router as example_router
 from .qna import router as qna_router
 from .rag_chat import router as rag_assistant_router
